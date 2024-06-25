b38070e4dd983f18035df41fb5921111f9703cdc
stevepbunston <60156504+stevepbunston@users.noreply.github.com>
2024-06-24 21:15:57 -0400

    adding keeper back up file

--- models/article.js
+++ models/article.js
@@ -2,79 +2,31 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Review = require('./review');
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+const RankedImageSchema = new Schema({
+    rank: Number,
+    name: String,
+    image: ImageSchema
+});
+
 const ArticleSchema = new Schema({
     title: String,
     game: String,
     release: String,
     description: String,
     blog: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     console: String,
-    movieOne: String,
-    movieOneImage: String,
-    movieTwo: String,
-    movieTwoImage: String,
-    movieThree: String,
-    movieThreeImage: String,
-    movieFour: String,
-    movieFourImage: String,
-    movieFive: String,
-    movieFiveImage: String,
-    musicOne: String,
-    musicOneImage: String,
-    musicTwo: String,
-    musicTwoImage: String,
-    musicThree: String,
-    musicThreeImage: String,
-    musicFour: String,
-    musicFourImage: String,
-    musicFive: String,
-    musicFiveImage: String,
-    nhlOne: String,
-    nhlOneImage: String,
-    nhlTwo: String,
-    nhlTwoImage: String,
-    nhlThree: String,
-    nhlThreeImage: String,
-    nhlFour: String,
-    nhlFourImage: String,
-    nhlFive: String,
-    nhlFiveImage: String,
-    nbaOne: String,
-    nbaOneImage: String,
-    nbaTwo: String,
-    nbaTwoImage: String,
-    nbaThree: String,
-    nbaThreeImage: String,
-    nbaFour: String,
-    nbaFourImage: String,
-    nbaFive: String,
-    nbaFiveImage: String,
-    mlbOne: String,
-    mlbOneImage: String,
-    mlbTwo: String,
-    mlbTwoImage: String,
-    mlbThree: String,
-    mlbThreeImage: String,
-    mlbFour: String,
-    mlbFourImage: String,
-    mlbFive: String,
-    mlbFiveImage: String,
-    nflOne: String,
-    nflOneImage: String,
-    nflTwo: String,
-    nflTwoImage: String,
-    nflThree: String,
-    nflThreeImage: String,
-    nflFour: String,
-    nflFourImage: String,
-    nflFive: String,
-    nflFiveImage: String,
+    movies: [RankedImageSchema],
+    music: [RankedImageSchema],
+    nhl: [RankedImageSchema],
+    nba: [RankedImageSchema],
+    mlb: [RankedImageSchema],
+    nfl: [RankedImageSchema],
     reviews: [
         {
             type: Schema.Types.ObjectId,
